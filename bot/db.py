fc24118d5cf4ebb4c196b22f2df9d8b276ea48ec
mlatham <latham.8@outlook.com>
2024-05-08 20:42:44 -0400

    docker compose db and db connection pooling

---
+++ bot/db.py
@@ -0,0 +1,21 @@
+import psycopg2
+import os
+
+
+class DB:
+    def __init__(self, minconn, maxconn):
+        self.conn_pool = psycopg2.pool.SimpleConnectionPool(
+            minconn,
+            maxconn,
+            database=os.environ.get('POSTGRES_DB'),
+            user=os.environ.get('POSTGRES_USER'),
+            password=os.environ.get('POSTGRES_PASSWORD'),
+            host=os.environ.get('POSTGRES_HOST'),
+            port=os.environ.get('POSTGRES_PORT')
+        )
+
+    def get_conn(self):
+        return self.conn_pool.getconn()
+
+    def release_conn(self, conn):
+        self.conn_pool.putconn(conn)
--- bot/main.py
+++ bot/main.py
@@ -20,4 +20,3 @@ async def on_ready():
 
 bot.run(TOKEN)
 
-34
