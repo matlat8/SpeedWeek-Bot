f65359493dd56142d1882a82b2c22acd6fd81817
mlatham <latham.8@outlook.com>
2024-07-02 20:26:48 -0400

    cars task using asyncpg. both tasks were locking the main thread i think

--- bot/core/crud/cars.py
+++ bot/core/crud/cars.py
@@ -1,9 +1,9 @@
 
 
-def upsert_cars(conn, cars):
+async def upsert_cars(conn, cars):
     insert_sql = """
     INSERT INTO cars (ir_id, car_make, car_model, car_name, car_name_abbreviated, car_categories, car_weight, free_with_subscription, has_headlights, has_multiple_dry_tire_types, has_rain_capable_tire_types, hp, is_ps_purchasable, price, rain_enabled)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
     ON CONFLICT (ir_id) DO UPDATE SET
     car_make = EXCLUDED.car_make,
     car_model = EXCLUDED.car_model,
@@ -20,7 +20,6 @@ def upsert_cars(conn, cars):
     price = EXCLUDED.price,
     rain_enabled = EXCLUDED.rain_enabled;
     """
-    with conn.cursor() as cursor:
-        for car in cars:
-            cursor.execute(insert_sql, (car['ir_id'], car['car_make'], car['car_model'], car['car_name'], car['car_name_abbreviated'], car['car_categories'], car['car_weight'], car['free_with_subscription'], car['has_headlights'], car['has_multiple_dry_tire_types'], car['has_rain_capable_tire_types'], car['hp'], car['is_ps_purchasable'], car['price'], car['rain_enabled']))
-        conn.commit()
+
+    for car in cars:
+        await conn.execute(insert_sql, car['ir_id'], car['car_make'], car['car_model'], car['car_name'], car['car_name_abbreviated'], car['car_categories'], car['car_weight'], car['free_with_subscription'], car['has_headlights'], car['has_multiple_dry_tire_types'], car['has_rain_capable_tire_types'], car['hp'], car['is_ps_purchasable'], car['price'], car['rain_enabled'])
